90d215816e4f2d6ba6d37c218c5dd97857736fcc
TejeshCodes <63721912+TejeshCodes@users.noreply.github.com>
2020-05-22 12:12:28 +0530

    Update box.js

--- box.js
+++ box.js
@@ -5,21 +5,21 @@ class Box {
           'friction':1.0,
           'density':1.0
       }
-      this.body = Bodies.rectangle(x, y, 40, 15, options);
-      this.width = 40;
-      this.height = 15;
+      this.body = Bodies.rectangle(x, y, 35, 50, options);
+      this.width = 35;
+      this.height = 50;
       
       World.add(world, this.body);
     }
     display(){
-      var pos =this.body.position;
+      var pos = this.body.position;
       var angle = this.body.angle;
       push();
       translate(pos.x, pos.y);
       rotate(angle);
       rectMode(CENTER);
       strokeWeight(4);
-      stroke("green");
+      stroke("red");
       fill(255);
       rect(0, 0, this.width, this.height);
       pop();
